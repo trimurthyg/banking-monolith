71fc1523b0736301efbb4982009cfa910112af4c
Trimurthy Reddy <trimurthyg.dev@gmail.com>
2026-03-01 02:32:54 +0530

    Initial commit

---
+++ src/main/java/com/bank/banking_monolith/BankingMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.bank.banking_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingMonolithApplication.class, args);
+	}
+
+}
